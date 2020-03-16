732c33c7fda480243b1fbf8a8d3216a5a876c85c
Maaikjuh <maaike.kraamer@hotmail.com>
2020-03-16 11:30:34 +0100

    added dof and mesh coordinates extraction

--- examples/systemic_circulation/leftventricular_meshing.py
+++ examples/systemic_circulation/leftventricular_meshing.py
@@ -6,37 +6,24 @@ This script provides an example on how to create and export a left ventricular m
 import time
 import numpy as np
 from dolfin.cpp.common import mpi_comm_world
-from dolfin.cpp.mesh import Mesh
+from dolfin.cpp.mesh import * 
 from dolfin import VectorElement, FunctionSpace
 from dolfin.cpp.io import HDF5File, XDMFFile
 from cvbtk import LeftVentricleGeometry, print_once, info_once, save_to_disk, quadrature_function_space
+import csv
 
 # We can inspect what parameters are passable to LeftVentricleGeometry:
 print_once('Passable parameters to LeftVentricleGeometry:')
 info_once(LeftVentricleGeometry.default_parameters(), True)
 
+# ik heb zelf alleen nog maar 'mesh resolution' aangepast, ik heb nog niet gekeken wat de andere parameters precies deden
 geometry_inputs = {
- #   'cavity_volume': 44.,  
     'focus_height': 4.3,  
-    'mesh_resolution': 30.,
+    'mesh_resolution': 20.,
     'inner_eccentricity': 0.934819,   
-    'mesh_segments': 30,  
+    'mesh_segments': 1,  
     'outer_eccentricity': 0.807075,  
     'truncation_height': 2.4}   
-#    'wall_volume': 136.,
- #   'load_fiber_field_from_meshfile':False} 
-
-# We can change the default meshing parameters by creating a dictionary and
-# passing that dictionary to geometry_inputs.
-#mesh_generator_parameters = {
-#'mesh_resolution': float(20),
-#'odt_optimize': True,
-#'lloyd_optimize': False,
-#'perturb_optimize': False,
-#'exude_optimize': False}
-
-# Add the mesh_generator parameters to the geometry inputs.
-#geometry_inputs['mesh_generator'] = mesh_generator_parameters
 
 # Create a mesh name.
 mesh_name = 'lv_maaike_seg{}_res{}'.format(geometry_inputs['mesh_segments'],
@@ -54,6 +41,28 @@ t0 = time.time()
 lv_mesh = lv.mesh()
 print_once('Mesh created in {} s.'.format(time.time()-t0))
 
+# Get the mesh coordinates. These are only printed to check the output and aren't stored (yet)
+mesh_points=lv_mesh.coordinates()
+print_once('coordinates: {} /n'.format(mesh_points))
+
+# Get the order of degrees of freedom of functions from functions spaces over the mesh
+# ik weet nog niet precies hoe dit precies werkt, of de 'CG' wel klopt en wat de dof mij precies zeggen 
+V = FunctionSpace(lv_mesh, "CG",2)
+n = V.dim()
+d = lv_mesh.geometry().dim()
+
+dof_coordinates = V.tabulate_dof_coordinates()
+print_once('dof_coordinates: {} /n'.format(dof_coordinates))
+dof_coordinates.resize((n, d))
+print_once('dof_coordinates resized: {} /n'.format(dof_coordinates))
+
+# Save the dof in a csv file
+with open("dof_coordinates.csv",'a',newline='') as outfile:
+    writer = csv.writer(outfile)
+    for row in dof_coordinates:
+            writer.writerow([row])
+
+
 # Specify whether to compute and save the fiber vectors.
 save_fiber_vector = None
 if save_fiber_vector:
@@ -68,7 +77,7 @@ else:
 lv.save_mesh_to_hdf5('output/leftventricular_mesh/{}_mesh.hdf5'.format(mesh_name),
                       save_fiber_vector=save_fiber_vector, V=V_q)
 
-#save the mesh to a XDMF file as well for easy viewing
+# Save the mesh to a XDMF file as well for easy viewing
 mesh_filename_hdf5 = ('output/leftventricular_mesh/{}_mesh.hdf5'.format(mesh_name)) 
 comm = mpi_comm_world()
 mesh = Mesh(comm)  # check mesh type                   
