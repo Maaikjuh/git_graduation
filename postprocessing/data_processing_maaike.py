fbea80fa66fbc2c55177a36ae6969c2413b14379
Maaikjuh <maaike.kraamer@hotmail.com>
2020-04-10 18:22:50 +0200

    added percentage comparision

--- postprocessing/data_processing_maaike.py
+++ postprocessing/data_processing_maaike.py
@@ -21,8 +21,8 @@ plt.close('all')
 #csv_normal = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\24-03_14-07_infarct_xi_10\results.csv'
 #csv_infarct = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\31-03_16-15_infarct_droplet_tao_20_meshres_30\results.csv'
 
-csv_normal = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\31-03_16-15_infarct_droplet_tao_20_meshres_30\results.csv'
-csv_infarct = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\07-04_15-23_fiber_reorientation_droplet_infarct_ta0_20_meshres_20\results.csv'
+csv_normal = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\24-03_14-07_infarct_xi_10\results.csv'
+csv_infarct = r'C:\Users\Maaike\Documents\Master\Graduation_project\Results_Tim\10-04_09-24_infarct_15_meshres_20\results.csv'
 
 
 ANALYZE = 'infarct'
@@ -34,10 +34,11 @@ results_infarct = Dataset(filename=csv_infarct)
 
 # Take the last cycle, or specify the cycle directly
 cycle = max(results_normal['cycle']) - 1
-cycle=10
+#cycle=10
 
-results_normal = results_normal[results_normal['cycle']==2]
-results_infarct = results_infarct[results_infarct['cycle']==10]
+
+results_normal = results_normal[results_normal['cycle']==cycle]
+results_infarct = results_infarct[results_infarct['cycle']==cycle]
 
 # Data from the selected cycle
 if ANALYZE == 'infarct':
@@ -47,12 +48,13 @@ else:
     data_cycle = results_normal
     pathname = csv_normal
 
-hemo_sum = hemodynamic_summary(data_cycle)
-print_hemodynamic_summary(hemo_sum,cycle)
+
 
 if COMPARE == True:
+    hemo_sum = procentual_change_hemodynamics(results_normal,results_infarct)
     plot_compare_results(results_normal,results_infarct, dir_out=os.path.dirname(csv_infarct), cycle=None)
 else:
+    hemo_sum = hemodynamic_summary(data_cycle)
+    print_hemodynamic_summary(hemo_sum,cycle)
     plot_results(data_cycle,dir_out=os.path.dirname(pathname), cycle=cycle)
     
-
