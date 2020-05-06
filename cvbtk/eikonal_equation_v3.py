79dde4e52ef43d2e487c0c7e68ca841017e1be51
Maaikjuh <maaike.kraamer@hotmail.com>
2020-05-06 17:16:09 +0200

    working boundary conditions

--- cvbtk/eikonal_equation_v3.py
+++ cvbtk/eikonal_equation_v3.py
@@ -52,74 +52,25 @@ def boundary(x, on_boundary):
     theta = compute_coordinate_expression(x,'theta',focus)
     xi = compute_coordinate_expression(x,'xi',focus)
     print(xi)
-    phi0 = 1/2*math.pi
-    phi1 = 1/4*math.pi
-    phi2 = 0.
+    phi0 = math.pi
+    phi1 = -1/2*math.pi
+    phi2 = 1/4*math.pi
     theta0 = 1/2*math.pi
 
     xi_max = 0.67
     xi_min = 0.371
 
-    tol = 1/20 * math.pi
+    tol = 1.e-1
+    tol_theta = 1.e-1
 
     on_xi_max = xi < xi_max + tol and xi > xi_max - tol
     on_xi_min = xi < xi_min + tol and xi > xi_min - tol
 
-    p0 = phi < phi0 + tol and phi > phi0 - tol and theta < theta0 + tol and theta > theta0 - tol 
-    p1 = phi < phi1 + tol and phi > phi1 - tol and theta < theta0 + tol and theta > theta0 - tol 
-    p2 = phi < phi2 + tol and phi > phi2 - tol and theta < theta0 + tol and theta > theta0 - tol 
+    p0 = phi < phi0 + tol and phi > phi0 - tol and theta < theta0 + tol_theta and theta > theta0 - tol_theta 
+    p1 = phi < phi1 + tol and phi > phi1 - tol and theta < theta0 + tol_theta and theta > theta0 - tol_theta 
+    p2 = phi < phi2 + tol and phi > phi2 - tol and theta < theta0 + tol_theta and theta > theta0 - tol_theta 
 
-    return (on_xi_min and (p0 or p1)) or (on_xi_max and p2) and on_boundary
-
-
-
-
-
-
-
-
-# def boundary(x, on_boundary ):
-#     a = Constant(4.3)
-#     sig = (1/(2*a))*( sqrt(x[0]*x[0]+x[1]*x[1]+(x[2]+a)*(x[2]+a))+ sqrt(x[0]*x[0]+x[1]*x[1]+(x[2]-a)*(x[2]-a)))
-#     tau = (1/(2*a))*( sqrt(x[0]*x[0]+x[1]*x[1]+(x[2]+a)*(x[2]+a))- sqrt(x[0]*x[0]+x[1]*x[1]+(x[2]-a)*(x[2]-a)))
-#     ksi = float(math.acosh(sig))
-#     phi = atan(x[1]/x[0])
-#     theta = float(acos(tau))
-#     x0 = 11.1/10
-#     y0 = -11.1/10
-#     z0 = -14/10
-#     x1 = 0.
-#     y1 = 16.3/10
-#     z1 = 0
-#     x2 = -16.3/10
-#     y2 = 0.
-#     z2 = 0
-#     x3 = -28.4/10
-#     y3 = 0
-#     z3 = -24/10
-#     x4 = 21.5/10
-#     y4 = 0
-#     z4 = 25/10
-#     r0 = sqrt((x[0]-x0)*(x[0]-x0)+(x[1]-y0)*(x[1]-y0)+(x[2]-z0)*(x[2]-z0))
-#     r1 = sqrt((x[0]-x1)*(x[0]-x1)+(x[1]-y1)*(x[1]-y1)+(x[2]-z1)*(x[2]-z1))
-#     r2 = sqrt((x[0]-x2)*(x[0]-x2)+(x[1]-y2)*(x[1]-y2)+(x[2]-z2)*(x[2]-z2))
-#     r3 = sqrt((x[0]-x3)*(x[0]-x3)+(x[1]-y3)*(x[1]-y3)+(x[2]-z3)*(x[2]-z3))
-#     r4 = sqrt((x[0]-x4)*(x[0]-x4)+(x[1]-y4)*(x[1]-y4)+(x[2]-z4)*(x[2]-z4))
-#     ksi_endo = 0.934819
-#     on_endo = abs(ksi - ksi_endo ) <= 1.e-3
-#     ksi_epi = 0.807075
-#     on_epi = abs(ksi - ksi_epi ) <=1.e-3
-#     r_act = 4/10
-#     within_r0 = r0<= r_act
-#     within_r1 = r1<= r_act
-#     within_r2 = r2<= r_act
-#     within_r3 = r3<= r_act
-#     within_r4 = r4<= r_act
-#     return ( on_endo and ( within_r0 or within_r1 or within_r2 )) or ( on_epi and within_r3 ) #or within_r4
-
-# phi = compute_coordinate_expression(degree, V,'phi',focus)
-# theta = compute_coordinate_expression(degree, V,'theta',focus)
-# xi = compute_coordinate_expression(degree, V,'xi',focus)
+    return (on_xi_max and p2) or (on_xi_min and (p0 or p1))   
 
 td0BC = Constant(0.0)
 bc = DirichletBC(V, td0BC , boundary, method ='pointwise')
