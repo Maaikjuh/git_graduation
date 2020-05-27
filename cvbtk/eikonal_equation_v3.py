a5679bba12672947dc84888adefd038fd1b15854
Maaikjuh <maaike.kraamer@hotmail.com>
2020-05-27 14:54:25 +0200

    working own geom, no diffusion along fibers?

--- cvbtk/eikonal_equation_v3.py
+++ cvbtk/eikonal_equation_v3.py
@@ -15,7 +15,10 @@ h5file.read(mesh , 'mesh', True )
 V = FunctionSpace(mesh , 'Lagrange',1)
 log(INFO , "Function Space dimension {}".format(V.dim ()))
 
-
+# Fibers
+VV = VectorFunctionSpace(mesh , 'Lagrange', 1, dim=3)
+fibers = Function(VV)
+h5file.read(fibers , 'fiber_vector')
 
 def compute_coordinate_expression(x,var,focus):
     """
@@ -70,41 +73,44 @@ def boundary(x, on_boundary):
     return (on_xi_max and p2) or (on_xi_min and (p0 or p1))   
 
 # Read inputs (we need the geometry inputs).
-inputs = read_dict_from_csv('inputs.csv')
+# inputs = read_dict_from_csv('inputs.csv')
 
-# Set the proper global FEniCS parameters.
-parameters.update({'form_compiler': inputs['form_compiler']})
+# # Set the proper global FEniCS parameters.
+# parameters.update({'form_compiler': inputs['form_compiler']})
 
-# Load geometry and first fiber field.
+# # Load geometry and first fiber field.
 
-inputs['geometry']['load_fiber_field_from_meshfile'] = True
-geometry = LeftVentricleGeometry(meshfile=filepath, **inputs['geometry'])
-# ef = geometry.fiber_vectors()[0].to_function(None)
-VV = VectorFunctionSpace(mesh , 'Lagrange', 1, dim=3)
-fibers = Function(VV)
-ef = geometry.fiber_vectors()[0].to_function(fibers)
-# geometry._fiber_vectors = None  # This may be a redundant statement (but I did not check if it works without).
-# geometry.load_fiber_field(filepath=filepath)
-# Create scalar function space for the difference in angle between vectors.
-Q = FunctionSpace(geometry.mesh(), 'Lagrange', 2)
+# inputs['geometry']['load_fiber_field_from_meshfile'] = True
+# geometry = LeftVentricleGeometry(meshfile=filepath, **inputs['geometry'])
+# # ef = geometry.fiber_vectors()[0].to_function(None)
+# VV = VectorFunctionSpace(mesh , 'Lagrange', 1, dim=3)
+# fibers = Function(VV)
+# ef = geometry.fiber_vectors()[0].to_function(fibers)
+# # geometry._fiber_vectors = None  # This may be a redundant statement (but I did not check if it works without).
+# # geometry.load_fiber_field(filepath=filepath)
+# # Create scalar function space for the difference in angle between vectors.
+# Q = FunctionSpace(geometry.mesh(), 'Lagrange', 2)
 
+# fiber_vectors = geometry.fiber_vectors()
+# [ef, es, en] = [np.asarray(_.to_array(None)) for _ in fiber_vectors] # NOTE: have shapes (3, -1)
+# ff = ([ef, es, en])
 # # Create vector function space.
 # # Fibers
 # VV = VectorFunctionSpace(mesh , 'Lagrange', 1, dim=3)
 # fibers = Function(VV)
 # ef = project(ef, VV)
-V1 = scalar_space_to_vector_space(Q)
+# V1 = scalar_space_to_vector_space(Q)
 # ef = project(ef, V1)
-save_to_disk(project(ef,V1), os.path.join(dir_out, 'ef.xdmf'))
-# ofile = XDMFFile(mpi_comm_world(), "fibers.xdmf")
-# ofile.write(ef)
-fibers = ef
+# save_to_disk(project(ef,V1), os.path.join(dir_out, 'ef.xdmf'))
+ofile = XDMFFile(mpi_comm_world(), "fibers.xdmf")
+ofile.write(fibers)
+# fibers = ef
 
 td0BC = Constant(0.0)
 bc = DirichletBC(V, td0BC , boundary, method ='pointwise')
 
 # Define parameters
-cm = Constant(5*10**-2)
+cm = Constant(5*10**-4)
 rho = Constant(1.4142)
 sig_il = Constant(2e-3)
 sig_it = Constant(4.16e-4)
@@ -125,10 +131,10 @@ w = TestFunction(V)
 td = Function(V)
 f = Constant(1)
 g = Constant(0)
-# ff = as_vector([ fibers[0], fibers[1], fibers[2]])
+ff = as_vector([ fibers[0], fibers[1], fibers[2]])
 # print(ff)
 # ff = fibers
-# ef = ff/sqrt(inner(ff ,ff))
+ef = ff/sqrt(inner(ff ,ff))
 I = Identity(3)
 Mi = sig_il * outer(ef ,ef)+ sig_it *(I- outer(ef ,ef))
 Me = sig_el * outer(ef ,ef)+ sig_et *(I- outer(ef ,ef))
@@ -166,8 +172,8 @@ log(INFO , "Solving eikonal equation")
 td.assign(td0)
 ofile = XDMFFile(mpi_comm_world(), os.path.join(dir_out,"Report_LV.xdmf"))
 it = 0
-dcm = ((6.7*10**-2)-(5*10**-2))/20
-while float(cm) <= (6.7*10**-2):
+dcm = ((6.7*10**-4)-(5*10**-4))/20
+while float(cm) <= (6.7*10**-4):
     print ("it={0}".format(it))
     print ("cm={0}".format(float(cm)))
     solver.solve()
